c80b508a6c0bf6196e6db8df4a4afcfbeeb45d80
Erik Browne <erik@browne.name>
2017-11-05 15:37:02 -0800

    Added unit tests.

--- app/src/test/java/com/example/erikbrowne/mvvmdemo/viewmodels/MainViewModelTest.kt
+++ app/src/test/java/com/example/erikbrowne/mvvmdemo/viewmodels/MainViewModelTest.kt
@@ -1,13 +1,19 @@
 package com.example.erikbrowne.mvvmdemo.viewmodels
 
+import android.app.Activity
 import android.app.Application
 import android.arch.core.executor.testing.InstantTaskExecutorRule
-import com.example.erikbrowne.mvvmdemo.mvvm.ViewMessagesEvent
-import com.nhaarman.mockito_kotlin.mock
+import android.content.Intent
+import android.net.Uri
+import com.example.erikbrowne.mvvmdemo.R
+import com.example.erikbrowne.mvvmdemo.mvvm.ViewMessages
+import com.example.erikbrowne.mvvmdemo.mvvm.ViewNavigation
+import com.nhaarman.mockito_kotlin.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
 import org.junit.Rule
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import org.junit.Test
 
 internal class MainViewModelTest {
 
@@ -17,15 +23,74 @@ internal class MainViewModelTest {
 
 	lateinit var viewModel: MainViewModel
 
-	@BeforeEach
+	private val message = "message"
+
+	private val application = mock<Application> {
+		on { getString(R.string.message_text) } doReturn message
+	}
+
+	@Before
 	fun beforeEachTest() {
-		viewModel = MainViewModel(mock<Application>())
+		viewModel = MainViewModel(application)
 	}
 
 	@Test
-	fun `showMessage sets message event`() {
+	fun `showMessage sends message event and shows message`() {
 		viewModel.showMessage()
 
-		assertTrue(viewModel.messageEvent.value is ViewMessagesEvent)
+		val event = viewModel.messageEvent.value
+		assertNotNull(event)
+		val viewMsgsObj = mock<ViewMessages>()
+		// the 'if' performs a smart cast
+		if ( event != null) {
+			viewMsgsObj.event()
+		}
+		verify(viewMsgsObj).showMessage(message)
 	}
+
+	@Test
+	fun `chooseFile sends nav event and starts activity`() {
+		viewModel.chooseFile()
+
+		val event = viewModel.navigationEvent.value
+		assertNotNull(event)
+		val viewNavObj = mock<ViewNavigation>()
+		// the 'if' performs a smart cast
+		if ( event != null) {
+			viewNavObj.event()
+		}
+		verify(viewNavObj).startActivityForResult(check {
+			assertEquals(Intent.ACTION_GET_CONTENT, it.action)
+			assertEquals("image/*", it.type)
+		}, any())
+	}
+
+	@Test
+	fun `processActivityResult success sets URI`() {
+		val uriStr = "content://something"
+		val uri = Uri.parse(uriStr)
+		val intent = mock<Intent> {
+			on { data } doReturn uri
+		}
+
+		viewModel.processActivityResult(REQUEST_CHOOSE_FILE, Activity.RESULT_OK, intent)
+
+		assertEquals(uriStr, viewModel.fileUri)
+	}
+
+	@Test
+	fun `processActivityResult canceled does nothing`() {
+		viewModel.processActivityResult(REQUEST_CHOOSE_FILE, Activity.RESULT_CANCELED, null)
+
+		assertEquals("", viewModel.fileUri)
+	}
+
+/*
+	@Test
+	fun `foo`() {
+		runBlocking {
+			viewModel.startTimer()
+		}
+	}
+*/
 }
